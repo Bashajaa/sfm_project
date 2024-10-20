cfa45bfbaa11e370827871cbabf46964b3b8329a
Bashajaa <hajni.baski@gmail.com>
2024-10-20 13:01:34 +0200

    Comic delete() 1 Commit

--- src/main/java/org/example/sfm_project/controller/ComicController.java
+++ src/main/java/org/example/sfm_project/controller/ComicController.java
@@ -3,6 +3,7 @@ package org.example.sfm_project.controller;
 import org.example.sfm_project.dtos.ComicDto;
 import org.example.sfm_project.service.ComicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,7 @@ public class ComicController {
 
     @PostMapping("/save")
     public void save(ComicDto comicDto){comicService.save(comicDto);}
+
+    @PostMapping("/delete/{comicId}")
+    public void delete(@PathVariable Integer comicId){comicService.delete(comicId);}
 }
