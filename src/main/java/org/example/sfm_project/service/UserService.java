9036d5aabae3e62066538d1d680a151fed06f0b6
Bashajaa <hajni.baski@gmail.com>
2024-10-20 02:08:30 +0200

    UserService save() Commit

--- src/main/java/org/example/sfm_project/service/UserService.java
+++ src/main/java/org/example/sfm_project/service/UserService.java
@@ -12,8 +12,13 @@ public class UserService {
     private UserRepository userRepository;
     public void save(UserDto userDto){
         User user = new User();
+        user.setUsername(userDto.getUsername());
         user.setName(userDto.getName());
         user.setEmail(userDto.getEmail());
+        user.setDateOfBirth(userDto.getDateOfBirth());
+        user.setGender(userDto.getGender());
+        user.setCountry(userDto.getCountry());
+        user.setRegistrationDate(userDto.getRegistrationDate());
         userRepository.save(user);
     }
 }
