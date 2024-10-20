01070654b3e11a6d94afee088bcbc2916c562f30
Bashajaa <hajni.baski@gmail.com>
2024-10-20 12:57:04 +0200

    Artist delete() 1 Commit

--- src/main/java/org/example/sfm_project/controller/ArtistController.java
+++ src/main/java/org/example/sfm_project/controller/ArtistController.java
@@ -3,6 +3,7 @@ package org.example.sfm_project.controller;
 import org.example.sfm_project.dtos.ArtistDto;
 import org.example.sfm_project.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,7 @@ public class ArtistController {
 
     @PostMapping("/save")
     public void save(ArtistDto artistDto){artistService.save(artistDto);}
+
+    @PostMapping("/delete/{artistId}")
+    public void delete(@PathVariable Integer artistId){artistService.delete(artistId);}
 }
