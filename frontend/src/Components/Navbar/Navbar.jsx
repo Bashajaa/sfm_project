7a16bb90a917da865443fb881c3c5ca28a9dea87
bogibalogh <baloghbogi200310@gmail.com>
2024-11-01 10:18:43 +0100

    navbar commit

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+import cart_logo from '../Assets/cart_logo.png'
+import profil from '../Assets/profil_icon.png'
+import searchIcon from '../Assets/search_button.jpg'
+
+export const Navbar = () => {
+
+  return (
+    <div className='navbar'> 
+
+    <div className='nav-logo'>
+        <h2>Comic Shop</h2> 
+    </div>
+
+    </div>
+  )
+}
