19c31e8c8a885f1f7a16bc0873b674cd410ffdd3
bogibalogh <baloghbogi200310@gmail.com>
2024-11-01 10:30:39 +0100

    login buttom

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -13,6 +13,19 @@ export const Navbar = () => {
         <h2>Comic Shop</h2> 
     </div>
 
+    <div className='nav-search'>
+        <input type="text" placeholder="Search..." />
+        <button type="submit" className="search-button">
+          <img src={searchIcon} alt="Search" />
+        </button>
+      </div>
+
+      <div className='nav-login-cart'>
+    <button><img src={profil} alt="profil" /></button>
+    <img src={cart_logo} alt="" />
+    <div className="nav-cart-count">0</div>
+    </div>
+
     </div>
   )
 }
