50c3f05c21b37eb93a88b5c04312e122de9ee6b2
bogibalogh <baloghbogi200310@gmail.com>
2024-11-01 11:17:44 +0100

    menu  buttom commit

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import './Navbar.css'
-
+import React, { useState } from 'react'; 
 import cart_logo from '../Assets/cart_logo.png'
 import profil from '../Assets/profil_icon.png'
 import searchIcon from '../Assets/search_button.jpg'
+import menu from '../Assets/menu.webp';
 
 export const Navbar = () => {
+
+    const [menuVisible, setMenuVisible] = useState(false); 
+
+    const toggleMenu = () => {
+      setMenuVisible(!menuVisible);
+    };
 
   return (
     <div className='navbar'> 
@@ -12,6 +19,23 @@ export const Navbar = () => {
     <div className='nav-logo'>
         <h2>Comic Shop</h2> 
     </div>
+
+    <div className='nav-menu'>
+        <button className='menu-button' onClick={toggleMenu}>
+          <img src={menu} alt="Menu" />
+        </button>
+        {menuVisible && ( 
+          <ul className='menu-list'>
+            <li>All Comic</li>
+            <li>Marvel</li>
+            <li>DC</li>
+            <li>Star Wars</li>
+            <li>European</li>
+            <li>Hungarian</li>
+            <li>Other</li>
+          </ul>
+        )}
+      </div>
 
     <div className='nav-search'>
         <input type="text" placeholder="Search..." />
