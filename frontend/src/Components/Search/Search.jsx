9e622b12133a69e69cfb508385602134ecd6f0b0
Viki <viktoria06@hotmail.hu>
2024-12-08 15:06:37 +0100

    Search css

--- frontend/src/Components/Search/Search.jsx
+++ frontend/src/Components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {useNavigate} from "react-router-dom";
+import "./Search.css"
 
 const SearchableList = () => {
     const [searchTerm, setSearchTerm] = useState("");
